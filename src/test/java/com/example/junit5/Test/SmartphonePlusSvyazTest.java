827b985df16264068cace5f39bcacc54a975fcf3
LoDFuR <LoDFuR1@gmail.com>
2025-01-27 14:16:13 +0300

    Added 1 test for the button 'Оформить'

--- src/test/java/com/example/junit5/Test/SmartphonePlusSvyazTest.java
+++ src/test/java/com/example/junit5/Test/SmartphonePlusSvyazTest.java
@@ -1,13 +1,17 @@
 package com.example.junit5.Test;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Driver;
 import com.codeborne.selenide.WebDriverRunner;
 import com.example.junit5.Pages.SmartphonePlusSvyaz;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriver.Window;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
@@ -20,4 +24,14 @@ public class SmartphonePlusSvyazTest {
         sleep(2000);
     }
 
+
+    @Test
+    public void ScrollToAccessoriesByTopButton(){
+        open(SmartphonePlusSvyaz.main);
+        Assertions.assertTrue(SmartphonePlusSvyaz.Button.exists());
+        SmartphonePlusSvyaz.Button.click();
+        SmartphonePlusSvyaz.TitleAccessories.shouldBe(Condition.visible, Duration.ofSeconds(3));
+        sleep(3000);
+        Assertions.assertTrue(SmartphonePlusSvyaz.TitleAccessories.isDisplayed());
+    }
 }
