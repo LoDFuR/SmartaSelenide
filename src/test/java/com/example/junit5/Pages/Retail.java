fcfbfef4218d4674003256281cfb47d0dffc2881
Марат Гиршон <leonardo_g@mail.ru>
2025-01-29 15:11:42 +0300

    Added skeleton of the regisrtation test for Retail.

--- src/test/java/com/example/junit5/Pages/Retail.java
+++ src/test/java/com/example/junit5/Pages/Retail.java
@@ -9,8 +9,10 @@ public class Retail {
 
     public static String url = "https://dev.smarta.lc/retail?orderSnapshotId=";
     public static SelenideElement ButtonStart = $(By.xpath("*//button[contains(text(), 'Начать')]"));
-    public static SelenideElement InputNumber = $(By.xpath("*//input[@name='phoneNumber']"));
+    public static SelenideElement InputNumber = $(By.xpath("(*//input[@name='phoneNumber'])[1]"));
     public static SelenideElement ButtonGetSMS = $(By.xpath("*//button[contains(text(), 'Получить смс-код')]"));
-
+    public static SelenideElement InputCode = $(By.xpath("(*//input[@id='codeInput'])[2]"));
+    public static SelenideElement ButtonSubmitCode = $(By.xpath("(*//button[@type = 'submit'])"));
     //public static SelenideElement ElementName = $(By.xpath("XPATH"));
+    //(*//input[@id='codeInput'])[2]
 }
--- src/test/java/com/example/junit5/Test/RetailTest.java
+++ src/test/java/com/example/junit5/Test/RetailTest.java
@@ -1,166 +1,145 @@
 package com.example.junit5.Test;
 //import net.lightbody.bmp.BrowserUpProxy;
-import com.browserup.bup.BrowserUpProxy;
-import com.browserup.bup.proxy.CaptureType;
-import com.browserup.harreader.model.HarEntry;
-import org.openqa.selenium.Proxy;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import com.codeborne.selenide.ClickOptions;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
 import com.example.junit5.Pages.Retail;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
-import org.openqa.selenium.logging.LogEntries;
-import org.openqa.selenium.logging.LogEntry;
-import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
-import java.util.List;
 import java.util.Set;
-import com.browserup.bup.assertion.model.AssertionResult;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.restassured.RestAssured.given;
+
 public class RetailTest {
-    /*
-     * Печатает куки ритейла до и после переоткрытия браузера
-     *  */
-    @Test
-    public void PrintCookiesForReOpeningPage() {
-        String orderID = "4a5e155d-b446-4daa-8c98-a82eebceb07d";
-        open(Retail.url + orderID);
-        sleep(2000);
-        Set<Cookie> cookies = getWebDriver().manage().getCookies();
-        //String localStorageContent = executeJavaScript("return JSON.stringify(localStorage);");
-        Cookie retail_acc_token = null;
-        // поиск retail_access_token
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("retail_access_token"))
-                retail_acc_token = cookie;
-
-            /*
-            // Cookie print
-            System.out.println("Name: " + cookie.getName());
-            System.out.println("Value: " + cookie.getValue());
-            System.out.println("Domain: " + cookie.getDomain());
-            System.out.println("Path: " + cookie.getPath());
-            System.out.println("Expiry: " + cookie.getExpiry());
-            System.out.println("Secure: " + cookie.isSecure());
-            System.out.println("HttpOnly: " + cookie.isHttpOnly());
-            System.out.println("-----------");
-            */
-        }
-
-        //Assertions.assertTrue(cookies.contains(), "retail_access_token cookie is missing");
-        //    System.out.println("Содержимое Local Storage: " + localStorageContent);
 
 
-        sleep(5000);
-        closeWebDriver();
-        // Reopen
-        open("https://dev.smarta.lc/retail?orderSnapshotId=" + orderID);
-        sleep(2000);
-        Set<Cookie> newCookies;
-        newCookies = getWebDriver().manage().getCookies();
-        // retail_access_token после закрытия WebDriver'а
-        for (Cookie cookie : newCookies) {
-            if (cookie.getName().equals("retail_access_token"))
-                Assertions.assertEquals(retail_acc_token, cookie);
-
-//            System.out.println("Name: " + cookie.getName());
-//            System.out.println("Value: " + cookie.getValue());
-//            System.out.println("Domain: " + cookie.getDomain());
-//            System.out.println("Path: " + cookie.getPath());
-//            System.out.println("Expiry: " + cookie.getExpiry());
-//            System.out.println("Secure: " + cookie.isSecure());
-//            System.out.println("HttpOnly: " + cookie.isHttpOnly());
-//            System.out.println("-----------");
-        }
-        // localStorageContent = executeJavaScript("return JSON.stringify(localStorage);");
-        //  System.out.println("Содержимое Local Storage: " + localStorageContent);
-        sleep(10000);
-    }
-
+/**
+ * 29.01 не проходила авторизация с помощью отправки запроса с помощью RESTASSURED
+ * /api/v1/user/registration/send-code - метод для получения кода при регистрации
+ * */
     @Test
     public void AccessTokenChangingAfterClosingBrowserTest() {
-//        ChromeOptions options = new ChromeOptions();
-//        options.setCapability("goog:loggingPrefs", Map.of("performance", "ALL"));
-//        ChromeDriverFactory()
-        String orderID = "4a5e155d-b446-4daa-8c98-a82eebceb07d";
+        String orderID = "8b7250e7-ef1b-47df-819a-877231779781";
 
-       // System.setProperty("chrome.exe", "C:\\Program Files\\Google\\Chrome\\Application");
-//        Configuration.proxyEnabled = true;
-//        // Настройка ChromeOptions для включения логирования производительности
-////        ChromeOptions options = new ChromeOptions();
-////        options.setCapability("goog:loggingPrefs", Map.of("CLIENT", "ALL"));
-////        options.setExperimentalOption("w3c", false);
-//        BrowserUpProxy bmp = WebDriverRunner.getSelenideProxy().getProxy();
-//       // bmp.start(0);
-//        // запоминать тело запросов (по умолчанию тело не запоминается, ибо может быть большим)
-//        bmp.setHarCaptureTypes(CaptureType.getAllContentCaptureTypes());
-//
-//        // запоминать как запросы, так и ответы
-//        bmp.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
-//
-//
-//        bmp.newHar("Q_Q");
+        // Добавляем режим инкогнито
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        Configuration.browserCapabilities = options;
 
         open(Retail.url + orderID);
         getWebDriver().manage().window().maximize();
-        //getWebDriver().manage().
+
         // Authorization
+        sleep(2000);
         Retail.ButtonStart.click(ClickOptions.withTimeout(Duration.ofSeconds(4)));
         sleep(1000);
         /*
          * Надо добавить автогенерацию номера
          * */
-        String number = "980-750-68-23";
+
+        String number = "980-750-68-24";
         Retail.InputNumber.setValue(number);
         Retail.ButtonGetSMS.click(ClickOptions.withTimeout(Duration.ofSeconds(3)));
-        sleep(4000);
-        // List<String> Logs = getWebDriverLogs(LogType.CLIENT);
-        //System.out.println(bmp.getHar());
-
-//        List<HarEntry> requests = bmp.getHar().getLog().getEntries();
-//        for (HarEntry HR : requests)
-//            System.out.println(HR.toString());
+        sleep(2000);
+        sleep(2000);
         /*
-        LogEntries performanceLog = getWebDriver().manage().logs().get(LogType.CLIENT);
-
-        // Анализируем логи на наличие POST запросов
-        for (LogEntry entry : performanceLog) {
-            String message = entry.getMessage();
-            if (message.contains("POST")) {
-                // Здесь вы можете обработать ответ
-                System.out.println("Ответ на POST запрос: " + message);
-            }
-        }
-*/
-//        for (String s : Logs)
-//            System.out.println("LOG" + s);
-            sleep(10000);
-            //Checking cookies
-
-        /*
-        Set<Cookie> newCookies = null;
-        Set<Cookie> cookies = null;
-        Cookie retail_acc_token = null;
+        REST ASSURE POST method
+            /api/v1/user/registration/send-code
+         */
+        RestAssured.baseURI = "https://dev.smarta.lc";
+        // Данные для отправки
+        // PhoneNumber = "+79807506824"
+        Set<Cookie> Coc;
+        Cookie retail_acc_token = null, acc_token = null;
+        Coc = getWebDriver().manage().getCookies();
         // поиск retail_access_token
-        for (Cookie cookie : cookies) {
+        for (Cookie cookie : Coc) {
             if (cookie.getName().equals("retail_access_token"))
                 retail_acc_token = cookie;
+            if (cookie.getName().equals("access_token")) ;
+            acc_token = cookie;
         }
-        cookies = getWebDriver().manage().getCookies();
+        String jsonBody = "{\"phone\":\"" + "+79807506824\"" + ", \"again\": false" +"}"; // + ", \"again\": false"
+        sleep(2000);
+        Response response = given().header("Content-Type", "application/json") // Установка заголовка Content-Type
+                .body(jsonBody) // Установка тела запроса
+                .sessionId(((RemoteWebDriver) getWebDriver()).getSessionId().toString()) // Попытка перенести данные сессии
+                .cookie(String.valueOf(retail_acc_token))
+                .when()
+                .post("/api/v1/user/registration/send-code") // Укажите путь для POST-запроса
+                .then()
+                .statusCode(200) // Ожидаем статус ответа 200
+                .extract()
+                .response(); // Извлечение ответа для дальнейшей обработки
+
+        // Печатаем ответ
+        response.getBody().prettyPrint();
+        // Анализ ответа
+        String responseBody = response.getBody().asString();
+       // System.out.println("Response Body: " + responseBody); // Выводим тело ответа
 
 
-        newCookies = getWebDriver().manage().getCookies();
-        // retail_access_token после закрытия WebDriver'а
-        for (Cookie cookie : newCookies) {
-            if (cookie.getName().equals("retail_access_token"))
-                Assertions.assertEquals(retail_acc_token, cookie);
-        }
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
 
-         */
+            // Преобразование строки JSON в JsonNode
+            JsonNode jsonNode = objectMapper.readTree(responseBody);
+            // Переводим json обратно в строку, чтобы получить "Внутренний" json
+            String data = jsonNode.get("data").toString();
+            System.out.println("Data: " + data); // Печатаем json ответа
+            JsonNode jNode = objectMapper.readTree(data);
+            // метод для получения значения не сработал -> получаю значение из строки ""%d""
+            String cc = jNode.get("code").toString();
+            System.out.println("Code as string: " + cc); // Печатаем код из смс
+            // Форматируем строку, удаляя "
+            cc = cc.replace("\"", "");
 
+
+            Retail.InputCode.setValue(cc);
+            sleep(900000);
+            Retail.ButtonSubmitCode.click(ClickOptions.withTimeout(Duration.ofSeconds(5)));
+            sleep(7000);
+
+
+            //Checking cookies
+
+            Set<Cookie> cookies = null;
+           // Cookie retail_acc_token = null, acc_token = null;
+            cookies = getWebDriver().manage().getCookies();
+            // поиск retail_access_token
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("retail_access_token"))
+                    retail_acc_token = cookie;
+                if (cookie.getName().equals("access_token")) ;
+                acc_token = cookie;
+            }
+            // печатаем куки access_token, retail_access_token
+            System.out.println(acc_token);
+            System.out.println("Name: " + acc_token.getName());
+            System.out.println("Value: " + acc_token.getValue());
+
+            System.out.println(retail_acc_token);
+            System.out.println("Name: " + retail_acc_token.getName());
+            System.out.println("Value: " + retail_acc_token.getValue());
+
+            sleep(10000);
+
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
+}
